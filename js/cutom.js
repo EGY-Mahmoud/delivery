c008e332cde7f69c0a310cd4a7daca4139bae420
EGY-Mahmoud <dmody773@gmail.com>
2023-01-08 04:45:34 +0200

    upload

---
+++ js/cutom.js
@@ -0,0 +1,32 @@
+
+
+// swiper slider
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+// swiper slider
+  var swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+        spaceBetween: 30,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+
+  });
+
+
+  // ==============================================
+  // hide buttons
+
